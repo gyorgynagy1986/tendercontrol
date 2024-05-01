634564400bf7b4491eeb620bcd807a1224aa2079
gyorgynagy1986 <gyorgynagy1986@gmail.com>
2024-05-01 11:39:26 +0200

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,35 @@
+
+// General
+
+export const alt = 'tender Comp Kft szeged közbeszerzés pályázat'
+export const button = 'kapcsolat'
+
+// Title
+
+export const titleH1 = 'Professzionális közbeszerzési szaktanácsadás' 
+
+
+// Nav
+
+export const nav = [
+    {name: "Szolgáltatásaink", link:'#'},
+    {name: "Rólunk", link:'#'},
+]
+
+// Services
+
+export const services = [
+   {name : 'Ajánlatkérők és ajánlattevők számára közbeszerzési - jogi tanácsadás', number: '#01', img: '/1.svg'} ,
+   {name : 'Közbeszerzési eljárások előkésztése, lebonyolítása', number: '#02', img: '/2.svg'} ,
+   {name : 'Közbeszerzési szabályzatok készítése', number: '#03', img: '/3.svg'} ,
+   {name : 'Ajánlatok összeállításában közreműködés', number: '#04', img: '/4.svg'} ,
+]
+
+// Sections
+
+export const sections = {
+    section1 : 'Szolgáltatásaink',
+    section2: 'Rólunk',
+    section3: 'Lépjen velünk kapcsolatba!'
+}
+
---
+++ src/layout/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Image from 'next/image'
+import Style from './Hero.module.css'
+import heroIamge from '../../public/assets/hero/hero.png'
+import Nav from '@/components/Nav/Nav'
+import {titleH1} from '@/data/data'
+
+const Hero = () => {
+  return (
+    <section className={Style.section}>
+       <Nav /> 
+      <Image priority alt='iamge' className={Style.heroImage} src={heroIamge} />
+          
+          <div className={Style.h1Container}><h1>Professzionális <br/> közbeszerzési szaktanácsadás</h1></div>
+    </section>
+  )
+}
+
+export default Hero
