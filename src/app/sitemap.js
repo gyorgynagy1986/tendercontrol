84acb7ed63c543df68116e2f98e7a3cf9533faa9
gyorgynagy1986 <gyorgynagy1986@gmail.com>
2024-05-09 15:01:13 +0200

    metadata

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+    return [
+      {
+        url: 'https://www.tendercontrol.hu/',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 1,
+      },
+    ]
+  }
